95b2a972e59a277197114d926468e4716b9a39b1
Dhruv Jalan <dhruvjalan0202@gmail.com>
2026-03-26 14:12:57 +0530

    Initial commit: Express boilerplate

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json()); 
+app.get('/', (req, res) => {
+    res.status(200).json({ 
+        status: "success", 
+        message: "Server is live! Hello World." 
+    });
+});
+
+// 4. Start the Server
+app.listen(PORT, () => {
+    console.log(`
+    Server is running on port: ${PORT}
+    URL: http://localhost:${PORT}
+    `);
+});
